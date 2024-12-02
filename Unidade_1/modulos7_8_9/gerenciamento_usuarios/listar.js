e4e5ae62003124ea96c8d7c40c32923b1cddcb05
Graniska <nataly.granisk@gmail.com>
2024-12-02 18:57:40 -0300

    gerenciamento usuarios added

---
+++ Unidade_1/modulos7_8_9/gerenciamento_usuarios/listar.js
@@ -0,0 +1,48 @@
+
+    const {usuarios} = require('./usuarios');
+
+        function listarUsers() {
+            
+            console.log('======Listagem======')
+
+                usuarios.forEach((objeto, index) => {
+                    
+                        console.log(`=========================
+                            |id:${objeto.id}
+                            |Nome: ${objeto.nome}
+                            |Email: ${objeto.email}
+                            |Telefones (${objeto.telefones.length}):  ${objeto.telefones}`)
+                });
+        }
+
+    module.exports = {listarUsers};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
